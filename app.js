4dff873d663ffbed9ed8f6bb4bb8d2ff03f0f3d3
Thomas Santos <thomas@socialeffort.co>
2017-10-09 20:09:17 -0400

    add test routes. change route from unsafe Chrome port

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 var express = require('express');
 var app = express();
 
+// home test
 app.get('/', function(req, res) {
   res.send('Drone synth firing..');
 });
 
+// deckard egg
 app.get('/deckard', function(req, res) {
   res.send('I had your job once. I was good at it.');
 });
 
-app.listen(2049, function() {
-  console.log('Scanning for replicants on LocalHost: 2049.');
+// officer K egg
+app.get('/officerk', function(req, res) {
+    res.send('I just want to ask some questions.');
+  });
+
+// console.log test
+app.listen(2048, function() {
+  console.log('Scanning for replicants on LocalHost: 2049 - 1.');
 })
