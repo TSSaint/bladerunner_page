737785e1ddf2dbf139b1d9abc520b967f7eab051
Thomas Santos <thomas@socialeffort.co>
2017-10-09 20:17:16 -0400

    add bladerunner and bladerunner 2049 routes

--- app.js
+++ app.js
@@ -13,8 +13,22 @@ app.get('/deckard', function(req, res) {
 
 // officer K egg
 app.get('/officerk', function(req, res) {
-    res.send('I just want to ask some questions.');
-  });
+  res.send('I just want to ask some questions.');
+});
+
+///////////////////////////
+// Blade Runner Original //
+//////////////////////////
+app.get('/bladerunner', function(req, res) {
+  res.send('LOS ANGELES: 1982.');
+});
+
+///////////////////////
+// Blade Runner 2049 //
+///////////////////////
+app.get('/bladerunner2049', function(req, res) {
+  res.send('LOS ANGELES: 2049.');
+});
 
 // console.log test
 app.listen(2048, function() {
